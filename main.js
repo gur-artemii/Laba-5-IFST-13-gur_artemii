102e34e8eb7093118e5aabf3a32bed28f304ed98
gur-artemii <korollike@mail.ru>
2025-06-06 23:26:21 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector("#burgerBtn");
-const mobileMenu= document.querySelector("#mobMenu");
-
-burger.addEventListener("click", ()=>{
-  mobileMenu.classList.toggle("hidden");
-});
-
-
