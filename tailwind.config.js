03747a03fccb1cfbc162ef3389318b7890fd436c
gur-artemii <korollike@mail.ru>
2025-06-06 23:25:42 +0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./webenar/css/style.css"],
-    theme: {
-      extend: {},
-    },
-    plugins: [require('tailwind-hamburgers')],
-  }
